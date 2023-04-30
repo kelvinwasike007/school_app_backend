7368e16c44ca961e993d9dbb4f37bd9b35065254
kevinwasike <kelvinwasike007@gmail.com>
2023-04-30 10:17:55 +0000

    forgot a  comma

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ async def app(scope, receive, send):
     ...
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0' port=80, log_level="info")
+    uvicorn.run("main:app", host='0.0.0.0', port=80, log_level="info")
