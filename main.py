ec75d089018180169c8501759644c90b4e7e4472
kevinwasike <kelvinwasike007@gmail.com>
2023-05-03 18:19:06 +0000

    Added CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import accounts, schools
 from mongoengine import connect
 import os
@@ -7,6 +8,14 @@ load_dotenv()
 db=os.getenv('DB_URL')
 connect('school', host=db)
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_credentials=True,
+    allow_headers=["*"]
+    )
 
 #School Management Routes
 app.include_router(schools.router)
