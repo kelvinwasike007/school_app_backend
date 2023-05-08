eaabc463d0be456d561062a10ac7940958db2cc7
kevinwasike <kelvinwasike007@gmail.com>
2023-05-08 18:04:36 +0000

    ADDED SMS AND MESSAGE TO NEW SCHOOL'S ADMIN ACC

--- api/views/school.py
+++ api/views/school.py
@@ -1,6 +1,12 @@
 from ..models.School import School
+from ..models.accounts import Account
 from mongoengine import ValidationError
+import random
+import string
+import hashlib
 import json 
+from ..helpers.sms import signup_msg
+from ..helpers.mail import signup_mail
 
 def add(school):
     errors = None
@@ -13,6 +19,22 @@ def add(school):
         new_school.schoolPhone = school.phone
         new_school.schooldomain = school.domain
         new_school.save()
+        #create a user account
+        admin = Account()
+        text_length = 10
+
+        # Generate a random text consisting of lowercase letters and digits
+        username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=text_length))
+        password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=text_length))
+
+        admin.username = username
+        admin.password = hashlib.md5(password.encode()).hexdigest()
+        admin.school = new_school
+        admin.role = 'admin'
+        admin.save()
+        #send new user account
+        signup_msg(school.phone, f"https://school-platform.wesempire.co.ke/{school.domain}", {'username':username, "password":password})
+        signup_mail([school.email], f"https://school-platform.wesempire.co.ke/{school.domain}", {"username":username, "password":password})
     except ValidationError as e:
         errors = e.message
     except Exception as e:
@@ -21,7 +43,7 @@ def add(school):
     
     if errors:
         return {'status':'error','error':errors}
-    return {"status":"School successfully registered"}
+    return {"status":"School successfully registered, Check Your Email and Phone For Further Instructions"}
 
 def update(updateInfo, schoolId):
     resSchool = School.objects(id=schoolId)
