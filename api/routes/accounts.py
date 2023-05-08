d06149ef5e3ee5012d606125b2c7439830afda8b
kevinwasike <kelvinwasike007@gmail.com>
2023-05-08 18:01:45 +0000

    AUTHENTICATION FOR ACCOUNTS ROUTES

--- api/routes/accounts.py
+++ api/routes/accounts.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from ..views.accounts import login, createaccount,updateaccount,deleteaccount,accountlist
 from .schools import updateSchema
 from pydantic import BaseModel
-
-
+from ..helpers.auth import logout
 
 router = APIRouter(
     prefix='/account',
@@ -24,17 +23,17 @@ def add_user(schoolId:str, acInfo:Account):
     """Create a user account"""
     return createaccount(schoolId, acInfo)
 
-@router.put('/update/{schoolId}/{acId}')
+@router.put('/update/{schoolId}/{acId}', dependencies=[Depends(logout)])
 def edit_user(schoolId:str, acId:str, updateInfo: updateSchema):
     """Updates a user info"""
     return updateaccount(schoolId, acId, updateInfo)
 
-@router.delete('/delete/{schoolId}/{acId}')
+@router.delete('/delete/{schoolId}/{acId}',dependencies=[Depends(logout)])
 def delete_user(schoolId:str, acId:str):
     """Deletes a user account"""
     return deleteaccount(schoolId, acId)
 
-@router.get('/{schoolId}')
+@router.get('/{schoolId}',dependencies=[Depends(logout)])
 def users(schoolId: str):
     """Get list of all accounts"""
     return accountlist(schoolId)
