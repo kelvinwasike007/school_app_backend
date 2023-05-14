c846e3221fcd2d55781c1fce9e389de3b1f7709e
kevinwasike <kelvinwasike007@gmail.com>
2023-05-14 17:24:23 +0000

    ROLES FIX

--- api/models/accounts.py
+++ api/models/accounts.py
@@ -1,15 +1,9 @@
 from mongoengine import *
-from enum import Enum
 from .School import School
-class Roles(Enum):
-    STAFF = 'staff'
-    ADMIN = 'admin'
-    PARENT = 'parent'
-    STUDENT = 'student'
 
 class Account(Document):
     """User Account"""
     username = StringField()
     password = StringField()
-    role = EnumField(Roles, default=Roles.STUDENT)
+    role = StringField()
     school = ReferenceField(School, required=True, reverse_delete_rule=CASCADE)
